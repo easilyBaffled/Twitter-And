d3a994f6057d60a947fd6ac286e7bca7553e77c5
easilyBaffled <dmichaelis0@gmail.com>
2014-02-20 21:19:16 -0500

    A lot of changes that don't change the look but make it better

--- index.js
+++ index.js
@@ -1,5 +1,105 @@
-$(function(){
-    //$(".add_button").click(add_timeline_element);
+$(function(){    
+    function add_timeline_element(){
+        var timeline_element_container = $('<li />');
+            var timeline_element = $('<ul />', {
+                class: "timeline_element"
+            });
+                var section_1 = $("<li />");
+                var section_2 = $("<li />");
+                    var close_button = $('<button />', {
+                        text: '-',
+                        class: "t_intect_button close_button",
+                        click: function(){$(this).parent().parent().remove();}
+                    });
+                    var search_bar = $('<input />', {
+                        type: "text",
+                        name: "search",
+                        class: "search_bar"
+                    });
+                    var search_button = $('<button />', {
+                        class: "search_button",
+                        text: "&#128269;"
+                    });
+
+                    section_1.append(close_button);
+                    section_1.append(search_bar);
+                    //section_2.append(search_button);
+                timeline_element.append(section_1);
+                timeline_element.append(section_2);
+        timeline_element_container.append(timeline_element);
+        $(".t_inject_container").append(timeline_element_container);
+    }
+    
+    function minimize_t_inject_container(){
+        $(".add_button").toggle();
+    }
+    
+    function create_twitter_bar(){
+        var table_container = $("<ul />", {
+            class: "t_inject_container"
+        });
+            var row = $("<li />", { class: "t_inject_row" });
+                var menu = $("<ul />", { class: "menu" });
+                    var section_1 = $("<li />");
+                    var section_2 = $("<li />");
+                        var add_element_button = $("<button />", {
+                            text: '+',
+                            click: add_timeline_element,
+                            class: "add_button t_intect_button"
+                            
+                        });
+                        var minimize_button = $("<button />", {
+                            text: 'm',
+                            click: minimize_t_inject_container,
+                            class: "minimize_button t_intect_button"
+                        });
+                    section_1.append(add_element_button);
+                    section_2.append(minimize_button);
+                menu.append(section_1);
+                menu.append(section_2);
+            row.append(menu);
+            table_container.append(row);
+        $('body').append(table_container);
+    }
+    create_twitter_bar();
+    
+    
+    
+    var scroll_point = 0;
+    var done_scrolling;
+    var counting = false;
+    var time_counter = 0.00;
+    
+    var scroll_ending = function () {
+        console.log("done scrolling");
+        clearInterval(counting);
+        counting = false;
+        time_counter = 0.00;
+    };
+    $(document).scroll(function() {
+        var new_scroll_point = $(document).scrollTop();
+        var scroll_delta = Math.abs(scroll_point - new_scroll_point);
+        /*if(scroll_delta < 0){
+            scroll_delta = scroll_delta * (-1);
+        }*/
+        scroll_point = new_scroll_point;
+        if(counting===false){
+            counting = setInterval(function(){
+                time_counter += 0.1;
+            }, 100);
+        } else{
+            var scroll_over_time = scroll_delta/time_counter;
+            console.log("scrolling over time:"+ scroll_over_time);
+            clearTimeout(done_scrolling);
+            done_scrolling = setTimeout(scroll_ending, 150);
+            if(scroll_over_time > 400 && scroll_over_time < 3000){
+                $(".add_button").hide();
+            }
+        }        
+    });
+});
+
+/*//$(".add_button").click(add_timeline_element);
     
     function add_timeline_element(){
         var text_input = $('<input />', {
@@ -51,92 +151,4 @@ $(function(){
         $('body').append(table_container);
     }
     create_twitter_bar();
-    
-    
-    
-    var scroll_point = 0;
-    var done_scrolling;
-    var counting = "false";
-    var time_counter = 0.00;
-    var consecutive_scrolls = 0.00;
-    
-    var scroll_ending = function () {
-        console.log("done scrolling");
-        clearInterval(counting);
-        counting = "false";
-        time_counter = 0.00;
-    }
-    $(document).scroll(function() {
-        var new_scroll_point = $(document).scrollTop();
-        var scroll_delta = scroll_point - new_scroll_point;
-        if(scroll_delta < 0){
-            scroll_delta = scroll_delta * (-1);
-        }
-        scroll_point = new_scroll_point;
-        if(counting=="false"){
-            counting = setInterval(function(){
-                time_counter += 0.1;
-            }, 100);
-        } else{
-            var scroll_over_time = scroll_delta/time_counter;
-            console.log("scrolling over time:"+ scroll_over_time);
-            clearTimeout(done_scrolling);
-            done_scrolling = setTimeout(scroll_ending, 150);
-            if(scroll_over_time > 400 && scroll_over_time < 3000){
-                $(".add_button").hide();
-            }
-        }        
-    });
-});
-                
-                       
-    /*
-    var count_down = "cleared";
-    var time_counter = 0;
-    
-    $(document).keydown(function(e){
-        if(e.which == 13){
-            if(count_down == "cleared"){
-                count_down = setInterval(function(){
-                                            time_counter++;
-                                            console.log("time:" + time_counter);
-                                        }, 1000);
-            console.log("--start--");
-            } else {
-                clearInterval(count_down);
-                count_down = "cleared";
-                time_counter = 0;
-                consecutive_scrolls = 0;
-                console.log("--stop--");
-            }
-        }
-    });
-    */
-    /*
-    var done_scrolling;
-    var consecutive_scrolls = 0.00;
-    var counting = "false";
-    var time_counter = 0.10;
-    $(window).bind('scroll',function () {
-        consecutive_scrolls += 1.00;
-        if(counting=="false"){
-            counting = setInterval(function(){
-                time_counter += 0.01;
-            }, 10);
-        }
-        var scroll_ratio = time_counter/consecutive_scrolls;
-        console.log('scroll_ratio: ' + scroll_ratio);
-        if(scroll_ratio < .030 && scroll_ratio > 0){
-            console.log("-----scrolling fast------");
-        }
-        clearTimeout(done_scrolling);
-        done_scrolling = setTimeout(scroll_ending, 100);
-    });
-    
-    var scroll_ending = function () {
-        console.log("done scrolling");
-        counting == "false";
-        consecutive_scrolls = 0.00;
-        time_counter = 0.00;
-    }
     */
